7c0771bf753cd3d98da63636fb6535681378feca
Chad Baker <calbaker@gmail.com>
2012-01-22 20:50:25 -0600

    better doc

--- constants.py
+++ constants.py
@@ -1,7 +1,8 @@
 """Module containing constants that are useful. 
-k_B : Boltzmann constant (kJ/K)
-Nhat : Avogadro's number (#/kmol)"""
+k_B : Boltzmann constant (kJ / K)
+Nhat : Avogadro's number (# / kmol)
+Rhat : Universal Gas Constant (kJ / kmol / K)"""
 
 k_B = 1.38e-26
 Nhat = 6.022e26
-
+Rhat = k_B * Nhat
