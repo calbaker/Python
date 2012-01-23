fixed problem with calculation of mu

properties.py
@@ -84,8 +84,8 @@ class ideal_gas(object):
         """Returns viscosity (Pa*s) of ideal gas attached from Bird,
         Stewart, Lightfoot Eq. 1.4-14.  This expression works ok for
         nonpolar gases, even ones with multiple molecules.""" 
-        mu = (5. / 16. * (np.pi * self.m * const.k_B * self.T)**0.5 /
-        (np.pi * self.d**2))   
+        mu = (5. / 16. * (np.pi * self.m * const.k_B * 1000. *
+        self.T)**0.5 / (np.pi * self.d**2))    
         return mu
 
     def get_c_p_air(self,T):